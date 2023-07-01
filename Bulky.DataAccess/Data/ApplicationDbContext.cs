a97a495482d46aa610543a3dba0a6a1c04a2afca
Franciszek Głąb <franciszek.glab@microsoft.wsei.edu.pl>
2023-07-01 17:33:57 +0200

    role

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -9,11 +9,12 @@ namespace Bulky.DataAcess.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
