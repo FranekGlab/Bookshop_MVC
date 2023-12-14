36d30650a21c33353f1d7dc5de0ec80f9c0b8bad
Franciszek Głąb <franciszek.glab@microsoft.wsei.edu.pl>
2023-12-14 23:39:54 +0100

    z

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -1,18 +1,47 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 
 namespace Bulky.Utility
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        private readonly string _apiKey = "9398bc0ba878b63999d4d83dce32ceb5";
+        private readonly string _apiSecret = "67b24cb150b41ed478bc30cdc489ac03";
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            //logic to send email
-            return Task.CompletedTask;
+            MailjetClient client = new MailjetClient(_apiKey, _apiSecret);
+
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+            .Property(Send.Messages, new JArray {
+                new JObject {
+                    {
+                        "From", new JObject {
+                            {"Email", "franciszekglab695@gmail.com"},
+                            {"Name", "Bulky Web"}
+                        }
+                    },
+                    {
+                        "To", new JArray {
+                            new JObject {
+                                {"Email", email}
+                            }
+                        }
+                    },
+                    {"Subject", subject},
+                    {
+                        "HTMLPart", htmlMessage
+                    }
+                }
+            });
+
+            await client.PostAsync(request);
         }
     }
 }
